aa46cf1a0e36d07ebcc43414e79f2c2c23ef6c13
Firas <firasmumtaz12@gmail.com>
2024-05-05 15:39:16 +0530

    initial next setup

---
+++ pages/_app.js
@@ -0,0 +1,55 @@
+import '../styles/globals.css'
+import { Provider } from '../context/context'
+import '@rainbow-me/rainbowkit/styles.css'
+import { chain, configureChains, createClient, WagmiConfig } from 'wagmi'
+
+import { polygon } from 'wagmi/chains';
+import { sepolia } from 'wagmi/chains';
+import { alchemyProvider } from 'wagmi/providers/alchemy';
+import { publicProvider } from 'wagmi/providers/public';
+
+import {
+  getDefaultWallets,
+  RainbowKitProvider,
+  darkTheme,
+} from '@rainbow-me/rainbowkit'
+
+import { infuraProvider } from 'wagmi/providers/infura'
+import { jsonRpcProvider } from 'wagmi/providers/jsonRpc'
+
+
+const { chains, provider, webSocketProvider } = configureChains(
+  [
+    polygon,
+    sepolia
+  ],
+  [
+    alchemyProvider({ apiKey: process.env.ALCHEMY_API_KEY }),
+    publicProvider()
+  ]
+);
+
+const { connectors } = getDefaultWallets({
+  appName: 'Blockbnb',
+  chains,
+})
+
+const wagmiClient = createClient({
+  autoConnect: false,
+  connectors,
+  provider,
+})
+
+const MyApp = ({ Component, pageProps }) => {
+  return (
+    <Provider>
+      <WagmiConfig client={wagmiClient}>
+        <RainbowKitProvider chains={chains} theme={darkTheme()} coolMode>
+          <Component {...pageProps} />
+        </RainbowKitProvider>
+      </WagmiConfig>
+    </Provider>
+  )
+}
+
+export default MyApp
